57f62b8597d91c16aace84864dd35b9904ffea9c
Nicola Mascolo <n.mascolopro@gmail.com>
2018-03-28 11:10:08 +0200

    A new challenger appears!

---
+++ Nicola/index.php
@@ -0,0 +1,32 @@
+<?php
+
+include('header.php');
+
+?>
+
+<form action="index.html" method="post">
+  <div>
+
+  </div>
+</form>
+
+
+  <table>
+    <tr>
+      <td>Adresse IP</td>
+      <td>Etat du serveur</td>
+      <td>Notation</td>
+    </tr>
+    <tr>
+      <td>192.0.0.1</td>
+      <td>UP</td>
+      <td>★★★☆☆</td>
+    </tr>
+  </table>
+
+
+<?php
+
+include('footer.php')
+
+ ?>
